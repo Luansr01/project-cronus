689e862a3c917445273dc0e31e5ca1a01e9012ee
Rafael-Alexandrini <ralexandrini2@gmail.com>
2025-09-06 05:00:51 -0300

    anima~çao slime

--- enemy_class.gd
+++ enemy_class.gd
@@ -2,6 +2,7 @@ class_name Enemy
 
 extends Node2D
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 @export var total_life: float
 var cur_life: float
@@ -17,5 +18,7 @@ func hit(dano_base: float, tipo : Elementos.Elems):
 		
 func _die():
 	print("morri!")
+	animated_sprite.play("dying")
+	await get_tree().create_timer(2).timeout
 	queue_free()
 	
